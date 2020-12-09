f313bf874640ef184fe17e14b298fedbf6d7efce
Buzzlay <dimabuslaev@gmail.com>
2020-12-09 15:25:31 +0500

    after review 09.12

--- homework.py
+++ homework.py
@@ -3,7 +3,6 @@ import os
 import time
 
 import requests
-from requests.exceptions import HTTPError
 import telegram
 from dotenv import load_dotenv
 
@@ -14,21 +13,33 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 PRAKTIKUM_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
-POLL_DICT = {
+REACTIONS = {
     'rejected': 'К сожалению в работе нашлись ошибки.',
-    'approved': 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+    'approved': 'Ревьюеру всё понравилось, '
+                'можно приступать к следующему уроку.'
 }
-MY_WARNING = 'Произошла ошибка при запросе через API, текст ошибки: '
-BOT_ERROR_TEXT = f'Бот столкнулся с ошибкой:'
-LOGGER = logging.getLogger('myLogger')
+MY_WARNING = 'Произошла ошибка при запросе через API, ' \
+             'неожиданный статус домашнего задания: "{status}"'
+BOT_ERROR_TEXT = 'Бот столкнулся с ошибкой: {exception}'
+BOT_ANSWER = 'У вас проверили работу "{name}"!\n' \
+             '\n{reaction}'
+CONNECTION_ERROR = ('Ошибка при отправке запроса: {error}\n'
+                    'URL запроса: {url}\n'
+                    'Параметры запроса:\n'
+                    'headers = {headers}\n'
+                    'params = {params}\n')
+PROCESSING_ERROR = ('Возникла ошибка при обработке запроса сервером,\n'
+                    'Текст ошибки: "{error}"')
+logger = logging.getLogger('myLogger')
 
 
 def parse_homework_status(homework):
-    homework_name = homework.get('homework_name')
-    if homework.get('status') in POLL_DICT.keys():
-        return f'У вас проверили работу "{homework_name}"!\n' \
-               f'\n{POLL_DICT[homework.get("status")]}'
-    return f'{MY_WARNING}{homework.get("error")}'
+    name = homework.get('homework_name')
+    status = homework['status']
+    if status in REACTIONS:
+        reaction = REACTIONS[status]
+        return BOT_ANSWER.format(args=[name, reaction])
+    raise MY_WARNING.format(status=status)
 
 
 def get_homework_statuses(current_timestamp):
@@ -39,17 +50,24 @@ def get_homework_statuses(current_timestamp):
             headers=HEADERS,
             params=params
         )
-    except HTTPError as http_err:
-        LOGGER.error(f'Ошибка при обработке запроса: {http_err}'
-                     f'Параметры запроса: '
-                     f'headers = {HEADERS}'
-                     f'params = {params}')
-
+    except requests.exceptions.RequestException as error:
+        raise ConnectionError(CONNECTION_ERROR.format(
+            error,
+            PRAKTIKUM_URL,
+            HEADERS,
+            params
+        ))
     else:
-        return response.json()
+        if 'error' in response.json():
+            raise IOError(PROCESSING_ERROR.format(error=response.json()['error']))
+        else:
+            return response.json()
 
 
-def send_message(message, bot_client=telegram.Bot(token=TELEGRAM_TOKEN)):
+def send_message(message, bot_client=None):
+    bot_client = telegram.Bot(
+        token=TELEGRAM_TOKEN
+    ) if bot_client is None else bot_client
     return bot_client.send_message(CHAT_ID, message)
 
 
@@ -66,8 +84,8 @@ def main():
                 current_timestamp
             )
             time.sleep(1200)
-        except Exception as e:
-            LOGGER.error(f'{BOT_ERROR_TEXT} {e}')
+        except Exception as exception:
+            logger.error(BOT_ERROR_TEXT.format(exception=exception))
             time.sleep(5)
 
 
